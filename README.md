bb68da24455fef68a184909e69d0962aea54a97c
skllzrmy.tez <joe@poundfit.com>
2023-03-30 17:39:57 -0700

    handling and responding to mentions, and setup for server deploy

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ The main functionality of the chatbot can be found in the `app.js` file. Running
 
 ## Arguments
 
-### The following arguments are available:
+### The following arguments are available for `app.js`:
 
 -   `--dev`: Run the chatbot in development mode. The toot will not be posted to Mastodon, and the console will show the generated toot or image URL instead.
 -   `--run-now`: Run the chatbot immediately, instead of waiting for the next interval.
@@ -34,9 +34,18 @@ The main functionality of the chatbot can be found in the `app.js` file. Running
 
 `node app.js --run-now --prompt "Write a toot about cute mastodons."` - Will generate a toot from the supplied prompt and post it, then continue looping on an interval. **The prompt will be used each loop**
 
+## Mention reply
+
+-   Bot can respond to direct messages from accounts it follows.
+-   Configure `MASTODON_ACCOUNT_ID` in .env file
+
+`npm run mentions` - Runs daemon that checks for new notifications every minute and responds if following user
+
+`npm run mentions-once` - Will check and respond to notifications once then exit
+
 ---
 
-## Deployment on Netlify
+## Deployment on Netlify -- (not currently using cloud deployment)
 
 -   Create a new site in Netlify from your Git repository.
 -   In the "Build & Deploy" settings for your site, set the "Build command" to npm run build.
@@ -46,7 +55,3 @@ The main functionality of the chatbot can be found in the `app.js` file. Running
 -   Once your build is complete, your Mastodon chatbot should be up and running on Netlify.
 
 ---
-
-## ToDo
-
--   Bot can respond to direct messages from accounts it follows.
---
+++ mentions.js
@@ -0,0 +1,129 @@
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+const M = require("mastodon");
+// const fs = require("fs");
+// const path = require("path");
+// const request = require("request");
+
+const mastodon = new M({
+    access_token: process.env.MASTODON_ACCESS_TOKEN,
+    api_url: process.env.MASTODON_API_URL,
+});
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const messages = [
+    {
+        role: "system",
+        content:
+            "You are chatGPToot! You are connected to a Mastodon social bot. Your current task is to respond to a direct mention, which will be posted to your Mastodon account. There is no human intervention. Limit your response to 500 characters.",
+    },
+];
+
+function checkMentions() {
+    mastodon
+        .get("notifications", {
+            types: ["mention"],
+        })
+        .then((resp_mentions) => {
+            console.log(resp_mentions.data);
+            // loop through each mention
+            for (var i in resp_mentions.data) {
+                mastodon
+                    .get("accounts/" + process.env.MASTODON_ACCOUNT_ID + "/following")
+                    .then((resp_following) => {
+                        console.log(resp_following.data);
+                        for (var i in resp_following.data) {
+                            if (resp_following.data[i].id === resp_mentions.data[i].account.id) {
+                                var mention = resp_mentions.data[i];
+                                var msgs = messages;
+                                // strip out all html elements from status.content and store in a new variable
+                                var status = mention.status.content.replace(/<[^>]*>?/gm, "");
+                                console.log(status);
+                                msgs.push(
+                                    {
+                                        role: "system",
+                                        content: "user " + mention.account.username + " mentioned you, saying:",
+                                    },
+                                    {
+                                        role: "user",
+                                        content: status,
+                                    }
+                                );
+                                openai
+                                    .createChatCompletion({ model: "gpt-3.5-turbo", messages: messages })
+                                    .then((resp_chatcomp) => {
+                                        var reply = resp_chatcomp.data.choices[0].message.content;
+                                        console.log(reply);
+                                        // post reply to Mastodon
+                                        mastodon
+                                            .post("statuses", {
+                                                status: reply,
+                                                visibility: "public",
+                                                in_reply_to_id: mention.status.id,
+                                            })
+                                            .then((result) => {
+                                                console.log("Reply successfully posted:", result.data.content);
+                                                mastodon
+                                                    .post("notifications/" + mention.id + "/dismiss")
+                                                    .then((result) => {
+                                                        console.log("Notification cleared.");
+                                                    })
+                                                    .catch((error) => {
+                                                        console.error(`Mastodon Error: ${error}`);
+                                                    });
+                                            })
+                                            .catch((error) => {
+                                                console.error(`Mastodon Error: ${error}`);
+                                            });
+                                    })
+                                    .catch((error) => {
+                                        console.log(error);
+                                    });
+                            } else {
+                                console.log("Not following user.");
+                                reply =
+                                    "I'm sorry, I'm not following you. I am only responding to mentions from users I am following.";
+                                mastodon
+                                    .post("statuses", {
+                                        status: reply,
+                                        visibility: "public",
+                                        in_reply_to_id: mention.status.id,
+                                    })
+                                    .then((result) => {
+                                        console.log("Reply successfully posted:", result.data.content);
+                                        mastodon
+                                            .post("notifications/" + mention.id + "/dismiss")
+                                            .then((result) => {
+                                                console.log("Notification cleared.");
+                                            })
+                                            .catch((error) => {
+                                                console.error(`Mastodon Error: ${error}`);
+                                            });
+                                    })
+                                    .catch((error) => {
+                                        console.error(`Mastodon Error: ${error}`);
+                                    });
+                            }
+                        }
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        return error;
+                    });
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+const args = process.argv.slice(2);
+const noLoop = args.includes("--no-loop");
+
+if (!noLoop) {
+    setInterval(() => {
+        checkMentions();
+    }, 15000);
+}
+checkMentions();
