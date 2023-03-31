2a56d246c5fbcba7d1fbae983bf9ab4256761695
skllzrmy.tez <joe@poundfit.com>
2023-03-30 18:28:25 -0700

    improved mention logging and handling of longer replies into multitoots

--- mentions.js
+++ mentions.js
@@ -27,7 +27,7 @@ function checkMentions() {
             types: ["mention"],
         })
         .then((resp_mentions) => {
-            console.log(resp_mentions.data);
+            console.log(resp_mentions.data.length + " mentions found at " + new Date());
             // loop through each mention
             for (var i in resp_mentions.data) {
                 mastodon
@@ -56,27 +56,78 @@ function checkMentions() {
                                     .then((resp_chatcomp) => {
                                         var reply = resp_chatcomp.data.choices[0].message.content;
                                         console.log(reply);
-                                        // post reply to Mastodon
-                                        mastodon
-                                            .post("statuses", {
-                                                status: reply,
-                                                visibility: "public",
-                                                in_reply_to_id: mention.status.id,
-                                            })
-                                            .then((result) => {
-                                                console.log("Reply successfully posted:", result.data.content);
+                                        var tootArray = false;
+                                        // if chatresponse is less than 500 characters, we can post it as is.
+                                        if (reply.length > 500) {
+                                            tootCount = Math.ceil(reply.length / 495);
+                                            tootArray = [];
+                                            for (let i = 0; i < tootCount; i++) {
+                                                const toot =
+                                                    reply.substring(i * 495, i * 495 + 495) +
+                                                    "[" +
+                                                    (i + 1) +
+                                                    "/" +
+                                                    tootCount +
+                                                    "]";
+                                                tootArray.push(toot);
+                                            }
+                                        }
+                                        if (tootArray) {
+                                            // post reply to Mastodon in multiple parts
+                                            for (let i = 0; i < tootArray.length; i++) {
                                                 mastodon
-                                                    .post("notifications/" + mention.id + "/dismiss")
+                                                    .post("statuses", {
+                                                        status: tootArray[i],
+                                                        visibility: "public",
+                                                        in_reply_to_id: mention.status.id,
+                                                    })
                                                     .then((result) => {
-                                                        console.log("Notification cleared.");
+                                                        if (i != tootArray.length) {
+                                                            console.log(
+                                                                "Reply part " + i + " successfully posted:",
+                                                                tootArray[i]
+                                                            );
+                                                        } else {
+                                                            console.log("Reply successfully posted:", tootArray[i]);
+                                                            mastodon
+                                                                .post("notifications/" + mention.id + "/dismiss")
+                                                                .then((result) => {
+                                                                    console.log(
+                                                                        "Notification " + mention.id + " cleared."
+                                                                    );
+                                                                })
+                                                                .catch((error) => {
+                                                                    console.error(`Mastodon Error: ${error}`);
+                                                                });
+                                                        }
                                                     })
                                                     .catch((error) => {
                                                         console.error(`Mastodon Error: ${error}`);
                                                     });
-                                            })
-                                            .catch((error) => {
-                                                console.error(`Mastodon Error: ${error}`);
-                                            });
+                                            }
+                                        } else {
+                                            // post reply to Mastodon
+                                            mastodon
+                                                .post("statuses", {
+                                                    status: reply,
+                                                    visibility: "public",
+                                                    in_reply_to_id: mention.status.id,
+                                                })
+                                                .then((result) => {
+                                                    console.log("Reply successfully posted:", result.data.content);
+                                                    mastodon
+                                                        .post("notifications/" + mention.id + "/dismiss")
+                                                        .then((result) => {
+                                                            console.log("Notification cleared.");
+                                                        })
+                                                        .catch((error) => {
+                                                            console.error(`Mastodon Error: ${error}`);
+                                                        });
+                                                })
+                                                .catch((error) => {
+                                                    console.error(`Mastodon Error: ${error}`);
+                                                });
+                                        }
                                     })
                                     .catch((error) => {
                                         console.log(error);
@@ -84,7 +135,7 @@ function checkMentions() {
                             } else {
                                 console.log("Not following user.");
                                 reply =
-                                    "I'm sorry, I'm not following you. I am only responding to mentions from users I am following.";
+                                    "I'm sorry, I'm not following you. I am only responding to mentions from users I am following. Please let us know if you would like to help us test this bot and how you would like to use it to be considered.";
                                 mastodon
                                     .post("statuses", {
                                         status: reply,
